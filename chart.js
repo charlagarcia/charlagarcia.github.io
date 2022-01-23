85d89c78891b5b6f67bea8a7e994ad1017ba1550
Charla Garcia <92953705+charlagarcia@users.noreply.github.com>
2022-01-22 20:17:27 -0600

    Update chart.js

--- chart.js
+++ chart.js
@@ -6,7 +6,7 @@ function init() {
   d3.json("samples.json").then((data) => {
     var sampleNames = data.names;
 
-    var sampleNames.forEach((sample) => {
+    sampleNames.forEach((sample) => {
       selector
         .append("option")
         .text(sample)
@@ -60,27 +60,27 @@ function buildCharts(sample) {
     // 3. Create a variable that holds the samples array. 
     var samples = data.samples;
     // 4. Create a variable that filters the samples for the object with the desired sample number.
-    var filteredSamples = samples.filter(sampleObj => sampleObj.id == sample);
+    var resultArray = samples.filter(sampleObj => sampleObj.id == sample);
     //  5. Create a variable that holds the first sample in the array.
-    var sample1 = filteredSamples[0];
+    var result = resultArray[0];
 
     // 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
-    var otuIds = result.otu_ids;
-    var otuLables = result.otu_labels;
-    var sampleValues = result.sample_values;
+    var otu_ids = result.otu_ids;
+    var otu_labels = result.otu_labels;
+    var sample_values = result.sample_values;
 
     // 7. Create the yticks for the bar chart.
     // Hint: Get the the top 10 otu_ids and map them in descending order  
     //  so the otu_ids with the most bacteria are last. 
 
-    var yticks = otuIds.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse();
+    var yticks = otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse();
 
     // 8. Create the trace for the bar chart. 
     var barData = [
       {
         y: yticks;
-        x: sampleValues.slice(0, 10).reverse(),
-        text: otuLables.slice(0, 10).reverse(),
+        x: sample_values.slice(0, 10).reverse(),
+        text: otu_lables.slice(0, 10).reverse(),
         type: "bar",
         orientation: "h",
       }
